fec8e5fe89f609db557c51ab2c1a6e0fc71cac82
alexandre_hamed <alexandrehamed@hotmail.fr>
2017-01-18 10:52:38 +0100

    index 1.1

--- index.php
+++ index.php
@@ -1,10 +1,94 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<!--[if lt IE 7 ]> <html lang="en" class="no-js ie6 lt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="no-js ie7 lt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="no-js ie8 lt8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="UTF-8" />
+  <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+  <title>Login and Registration Form with HTML5 and CSS3</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+  <meta name="keywords" content="html5, css3, form, switch, animation, :target, pseudo-class" />
+  <meta name="author" content="Codrops" />
+  <link rel="shortcut icon" href="../favicon.ico">
+  <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link href="https://fonts.googleapis.com/css?family=Passion+One|Cinzel" rel="stylesheet">
 
-  </body>
+
+
+
+</head>
+
+
+
+
+
+<body>
+
+<div class="site-wrapper">
+
+  <div class="site-wrapper-inner">
+
+    <div class="container-fluid">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-xs-0 col-md-offset-2 col-xs-offset-0">
+            <div class="masthead clearfix">
+              <div class="inner">
+                <h3 class="masthead-brand">Steam</h3>
+                <nav>
+                  <ul class="nav masthead-nav">
+                    <li class="active"><a href="#">Accueil</a></li>
+                    <li><a href="#">Magasin</a></li>
+                    <li><a href="#">Bibliotheque</a></li>
+                    <li><a href="#">panier</a></li>
+                    <li><a href="#">Deconnection</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-2 center">
+            <img class="img-responsive form_img_1" src="img/battlefield_1.png">
+
+            <h3 id="sub_img">Actualités</h3>
+          </div>
+          <div class="col-md-4  col-md-offset-2 center"
+              <div class="inner cover">
+                <h1 class="cover-heading form-h1">STEAM</h1>
+                <p class="lead subtitles_1">" Bienvenue sur steam la plateforme de jeux la plus utilisé par les joueurs "</p>
+                <p class="lead">
+                  <div class="row">
+                  <div class="col-xs-5 col-xs-offset-1"><a href="#" class="btn btn-lg btn-default">Bibliotheque</a></div>
+                  <div class="col-xs-5"><a href="#" class="btn btn-lg btn-default">Magasin</a></div>
+                  </div>
+                </p>
+              </div>
+          </div>
+      </div>
+
+
+    </div>
+
+  </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
 </html>
